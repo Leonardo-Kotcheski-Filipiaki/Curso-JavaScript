79c9237369ed39b9ae24354076977b3308c6867c
Leonardo Kotcheski Filipiaki <duckgames875@gmail.com>
2023-01-30 17:26:27 -0300

    Third Commit

---
+++ Seção sobre dados e operadores logicos/Aulas sobre Automatic Type Conversion/1_ATC.js
@@ -0,0 +1,10 @@
+console.log(5*null);//0.
+console.log("5" - 3);//2, converte o 5 em numero e faz o calculo, retornando number 2.
+console.log("5" + 1);//"51", converte o 1 em String e faz uma concatenação de Strings, ficando a String "51".
+console.log("dois"*"três")// NaN, não estão sendo calculados numeros!
+
+//OUTROS EXEMPLOS:
+
+console.log("120"+1)//1201 (String).
+console.log("120"-1)//119 (Number).
+console.log("120" * 1)//120, converte o 120 em type number e faz o calculo
---
+++ Seção sobre dados e operadores logicos/Aulas sobre Boolean/4_Boolean Operador Ternário/4_boolean.js
@@ -0,0 +1,13 @@
+//OPERADOR TERNÁRio, RETORNA O VALOR DEPENDENDO DA COMPARAÇÂO, EXEMPLO ABAIXO:
+console.log(8>10? 1:2);
+/**
+ * Nesse caso é comparado se 8 é maior que 10, se o resultado retornar TRUE, então será retornado
+ * o primeiro numero no caso o numero 1 e caso retorne FALSE ele retornara o segundo numero, no caso
+ * o numero 2. 
+ * */ 
+//MAIS UM EXEMPLO ABAIXO:
+console.log(8>10 ? "8>10 é TRUE":"8>10 é FALSE");
+/**
+ * Nesse caso é feita a mesma comparação do teste anterior mas agora expressando resultados em
+ * String.
+ */
